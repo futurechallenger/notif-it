ec448a8e5511d6964123549c3818afffbc30e062
Bruce Li <bruce.li@ringcentral.com>
2020-02-27 15:21:27 +0800

    use client side imp

--- client/src/services/subscribeEvents.ts
+++ client/src/services/subscribeEvents.ts
@@ -1,5 +1,18 @@
-function subscribeEvents(events: string[]) {
+async function subscribeEvents(events: string[]) {
   console.log('Subscribed events', events);
+  console.log('Trello ins', Trello);
+  try {
+    const ret = await Trello.put('webhooks', {
+      idModel: events[0],
+      description: '',
+      callbackURL: 'https://j-int.herokuapp.com/trello/hook',
+    });
+
+    console.log('==>RET', ret);
+  } catch (e) {
+    console.error('ERROR: ', e);
+  }
 }
 
 export { subscribeEvents };
+// https://api.trello.com/1/webhooks
--- client/typings/index.d.ts
+++ client/typings/index.d.ts
@@ -2,4 +2,22 @@ declare let Trello: {
   members: any;
 
   authorize(config: any);
+  get(
+    path: string,
+    params?: any,
+    success?: (params: any) => void,
+    error?: (e: any) => void,
+  );
+  post(
+    path: string,
+    params?: any,
+    success?: (params: any) => void,
+    error?: (e: any) => void,
+  );
+  put(
+    path: string,
+    params?: any,
+    success?: (params: any) => void,
+    error?: (e: any) => void,
+  );
 };
