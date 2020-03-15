7cc7dae52583276bfcff92e11a92e7bd7e9798c2
Bruce Li <bruce.li@BrucedeMacBook-Pro.local>
2020-03-15 09:07:49 +0800

    try another service

--- src/app.ts
+++ src/app.ts
@@ -20,59 +20,57 @@ const host =
     ? 'http://localhost:8333'
     : process.env.PROJECT_DOMAIN;
 
-// const serviceHook: HookService = new TrelloHookService();
-// const eventHandler: EventService = new TrelloEventService();
-// const messageService: MessageService = new TrelloMessageService();
-// const app = createApp(
-//   {
-//     service: 'trello',
-//     authType: 'implicit',
-//     serviceURL: 'https://api.trello.com',
-//     authURL: 'https://api.trello.com/1/authorize',
-//     hostURL: host,
-//     scopes: ['read'],
-//     returnURL: `${host}/callback`,
-//     responseType: 'token',
-//     clientId: process.env.TRELLO_KEY, // TODO: according to service of current auth
-//     clientIDAlias: 'key',
-//     name: 'The Awesome Notification App',
-//     expiration: 'never',
-//   },
-//   serviceHook,
-//   eventHandler,
-//   messageService,
-// );
-
-const serviceHook: HookService = new GithubHookService();
-const eventHandler: EventService = new GithubEventService();
-const messageService: MessageService = new GithubMessageService();
+const serviceHook: HookService = new TrelloHookService();
+const eventHandler: EventService = new TrelloEventService();
+const messageService: MessageService = new TrelloMessageService();
 const app = createApp(
   {
-    service: 'github',
-    authType: 'oauth2.0',
-    authURL: 'https://github.com/login/oauth/authorize',
-    tokenURL: 'https://github.com/login/oauth/access_token',
-    serviceURL: 'https://api.github.com',
+    service: 'trello',
+    authType: 'implicit',
+    serviceURL: 'https://api.trello.com',
+    authURL: 'https://api.trello.com/1/authorize',
     hostURL: host,
-    scopes: [
-      'admin:org',
-      'admin:org_hook',
-      'admin:repo_hook',
-      'repo_deployment',
-    ],
-    scopeDivider: ' ',
+    scopes: ['read'],
     returnURL: `${host}/callback`,
-    returnURLAlias: 'redirect_uri',
-    // responseType: 'token',
-    clientId: process.env.GITHUB_KEY, // TODO: according to service of current auth
+    responseType: 'token',
+    clientId: process.env.TRELLO_KEY, // TODO: according to service of current auth
+    clientIDAlias: 'key',
     name: 'The Awesome Notification App',
-    // expiration: 'never',
-    allowSignup: 'true',
+    expiration: 'never',
   },
   serviceHook,
   eventHandler,
   messageService,
 );
+
+// const serviceHook: HookService = new GithubHookService();
+// const eventHandler: EventService = new GithubEventService();
+// const messageService: MessageService = new GithubMessageService();
+// const app = createApp(
+//   {
+//     service: 'github',
+//     authType: 'oauth2.0',
+//     authURL: 'https://github.com/login/oauth/authorize',
+//     tokenURL: 'https://github.com/login/oauth/access_token',
+//     serviceURL: 'https://api.github.com',
+//     hostURL: host,
+//     scopes: [
+//       'admin:org',
+//       'admin:org_hook',
+//       'admin:repo_hook',
+//       'repo_deployment',
+//     ],
+//     scopeDivider: ' ',
+//     returnURL: `${host}/callback`,
+//     returnURLAlias: 'redirect_uri',
+//     clientId: process.env.GITHUB_KEY, // TODO: according to service of current auth
+//     name: 'The Awesome Notification App',
+//     allowSignup: 'true',
+//   },
+//   serviceHook,
+//   eventHandler,
+//   messageService,
+// );
 
 const port = process.env.PORT || 8333;
 
